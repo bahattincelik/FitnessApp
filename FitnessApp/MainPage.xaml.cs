9b57684a6aff536508811177fd3c3924cec47dd8
Bahattin Celik <celik@wave-solutions.de>
2024-09-05 11:52:23 +0200

    erfolgreich

---
+++ FitnessApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace FitnessApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnProfileClicked(object sender, EventArgs e)
+        {
+            // Profile sayfasına git
+            await Shell.Current.GoToAsync("//Profile");
+        }
+    }
+}
---
+++ FitnessApp/ProfilePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FitnessApp
+{
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSaveClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Saved", "Your profile has been saved.", "OK");
+        }
+    }
+}
